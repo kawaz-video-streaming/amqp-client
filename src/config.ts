33909af2882825cdaf04afeb2f4998adfeec1578
IdoKawaz <idohaker@gmail.com>
2026-04-22 09:59:01 +0300

    chore: update version to 7.3.1 and add AMQP_PREFETCH_COUNT to AmqpConfig

--- src/config.ts
+++ src/config.ts
@@ -2,16 +2,20 @@ import { z } from 'zod';
 
 export interface AmqpConfig {
     amqpConnectionString: string;
+    amqpPrefetchCount?: number;
 }
 
 
 const amqpEnvSchema = z.object({
     AMQP_CONNECTION_STRING: z.url({ protocol: /^amqp(s)?$/ }),
+    AMQP_PREFETCH_COUNT: z.coerce.number().optional(),
+
 });
 
 export const createAmqpConfig = (): AmqpConfig => {
     const amqpEnv = amqpEnvSchema.parse(process.env);
     return {
-        amqpConnectionString: amqpEnv.AMQP_CONNECTION_STRING
+        amqpConnectionString: amqpEnv.AMQP_CONNECTION_STRING,
+        amqpPrefetchCount: amqpEnv.AMQP_PREFETCH_COUNT
     };
 }
